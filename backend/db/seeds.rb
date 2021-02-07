271c8618998c0437340c38c9cecaa7f88ae28d8b
Teodoro Garcia <tedtke514@gmail.com>
2021-02-06 22:08:24 -0600

    added seed files to db

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Post.delete_all
+Comment.delete_all
+# post seeds
+post1 = Post.create(content: "Hello, this is a post.")
+post2 = Post.create(content: "Hello, this is a second post.")
+
+# comment seeds
+comment1 = Comment.create(content: "Hello, this is a comment.", post_id: post1.id)
+comment2 = Comment.create(content: "Hello, this is a second comment.", post_id: post2.id)
