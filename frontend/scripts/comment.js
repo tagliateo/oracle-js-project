2084222faf365f7762539f0bf8a0526b2fd4310c
Teodoro Garcia <tedtke514@gmail.com>
2021-02-06 23:24:46 -0600

    added dom manipulaiton to display comment

--- frontend/scripts/comment.js
+++ frontend/scripts/comment.js
@@ -6,5 +6,28 @@ class Comment {
         this.id = comment.id
     }
 
-   
+    static createComment(e){
+        e.preventDefault()
+        const commentInput = e.target.children[0].value
+        const commentList = e.target.nextElementSibling
+        const postId = e.target.parentElement.dataset.id
+
+        Comment.submitComment(commentInput, commentList, postId)
+    
+        e.target.reset()
+    }
+    
+    renderComment(commentList){
+        const li = document.createElement('li')
+        li.dataset.id = this.post_id
+        li.innerText = this.content
+    
+        const deleteBtn = document.createElement('button')
+        deleteBtn.innerText = "X"
+        li.appendChild(deleteBtn)
+        commentList.appendChild(li)
+    
+    }
+    
+
 }
