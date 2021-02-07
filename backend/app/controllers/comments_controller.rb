75a55309c015c38b8e5363e89e6dc26b3f22782a
Teodoro Garcia <tedtke514@gmail.com>
2021-02-06 22:19:45 -0600

    added controller functinality to comment

--- backend/app/controllers/comments_controller.rb
+++ backend/app/controllers/comments_controller.rb
@@ -1,2 +1,31 @@
 class CommentsController < ApplicationController
+    before_action :set_comment, only: [:destroy]
+
+    def index 
+        render json: Comment.all
+    end 
+
+    def create 
+        comment = Comment.new(comment_params)
+
+        if comment.save 
+            render json: comment 
+        else 
+            byebug 
+        end 
+    end 
+
+    def destroy 
+        comment.destroy
+    end
+
+    private
+
+    def set_comment 
+        comment = Comment.find(params[:id])
+    end 
+
+    def comment_params 
+        params.require(:comment).permit(:content, :post_id)
+    end 
 end
